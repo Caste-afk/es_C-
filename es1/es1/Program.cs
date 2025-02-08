3d590688a09669ba5407ce746cbe2f45024d4fa8
CASTEGNARO EROS <10933991@itisrossi.edu.it>
2025-02-08 09:25:59 +0100

    es1

---
+++ es1/es1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Inserisci un numero: ");
+int numero = int.Parse(Console.ReadLine());
+for (int i =1; i < numero; i++)
+{
+    Console.Write(i +",");
+}
